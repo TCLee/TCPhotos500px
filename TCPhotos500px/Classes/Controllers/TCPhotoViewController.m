4a7168853c0bfc7669204a340cfb386343b2b365
Lee Tze Cheun <leetzecheun@gmail.com>
2013-07-24 14:12:19 +0800

    Resized modal view and removed shadow and rounded borders.

--- TCPhotos500px/Classes/Controllers/TCPhotoViewController.m
+++ TCPhotos500px/Classes/Controllers/TCPhotoViewController.m
@@ -11,6 +11,8 @@
 
 #import "UIImageView+WebCache.h"
 
+#import <QuartzCore/QuartzCore.h>
+
 @interface TCPhotoViewController ()
 
 @property (nonatomic, weak) IBOutlet UIImageView *imageView;
@@ -23,24 +25,36 @@
 
 #pragma mark - View Life Cycle
 
-- (void)viewDidLoad
+- (void)viewWillAppear:(BOOL)animated
 {
-    [super viewDidLoad];
-    
-	[self reloadView];
+    [super viewWillAppear:animated];
+        
+    // Remove rounded corner and shadow from the modal view.
+    self.view.layer.cornerRadius = 0.0f;
+    self.view.layer.shadowOpacity = 0.0f;
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
     
+#warning Hard-coded values. Should calculate size based on device orientation.
+    // The bounds of the modal's superview determines the size of our content view
+    // presented on the screen. Also set it in viewDidAppear, otherwise it will not take effect.
+    // http://stackoverflow.com/a/4271364
+    self.view.superview.bounds = CGRectMake(0.0f, 0.0f, 700.0f, 650.0f);
+    
     // Add tap gesture recognizer in viewDidAppear because our view is now added
     // to the window.
     [self addTapToDismissGesture];
+    
+    // Load the image asynchronously and display it on this view.
+    [self configureView];
 }
 
 #pragma mark - Dismiss Modal View on Tap Gesture
 
+// StackOverflow: Tap to Dismiss http://stackoverflow.com/a/6180584
 - (void)addTapToDismissGesture
 {
     UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapToDismiss:)];
@@ -58,7 +72,10 @@
 {
     if (UIGestureRecognizerStateRecognized == sender.state) {
         [self.view.window removeGestureRecognizer:sender];
-        [self dismissViewControllerAnimated:YES completion:^{
+        
+        // Automatically forwards message to the presenting view controller (parent)
+        // to dismiss this presented view controller (child).
+        [self dismissViewControllerAnimated:NO completion:^{
             
         }];
     }
@@ -71,14 +88,23 @@
     if (_photo != newPhoto) {
         _photo = newPhoto;
         
-        [self reloadView];
+        [self configureView];
     }
 }
 
-/* Updates the view with the photo model's data. */
-- (void)reloadView
+// Updates the view with the photo model's data.
+- (void)configureView
 {
-    [self.imageView setImageWithURL:self.photo.imageURL];
+    //TODO: Perhaps show the thumbnail while we load the actual image?
+    [self.imageView setImageWithURL:self.photo.imageURL placeholderImage:nil options:0 completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType) {
+//        NSLog(@"Image Size = %@", NSStringFromCGSize(image.size));
+//        
+//        if (image) {
+//            CGSize imageSize = image.size;
+//            self.view.superview.bounds = CGRectMake(0, 0, imageSize.width * 0.8, imageSize.height * 0.8);
+//        }
+    }];
+    
     self.titleLabel.text = self.photo.title;
     self.fullNameLabel.text = self.photo.userFullName;
 }
